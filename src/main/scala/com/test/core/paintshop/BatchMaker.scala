72e8ef9f4d7ca93281b124ba651c78eab9879f64
Soluyanov <soluyanov.a.v@gmail.com>
2017-09-18 15:49:54 +0300

    start

--- src/main/scala/com/test/core/paintshop/BatchMaker.scala
+++ src/main/scala/com/test/core/paintshop/BatchMaker.scala
@@ -1,5 +1,8 @@
 package com.test.core.paintshop
 
+import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
 object BatchMaker {
 
   def main(args: Array[String]) {
@@ -10,6 +13,24 @@ object BatchMaker {
           "Provide input file path")
       System.exit(1)
     }
+
+    /**
     XSLTransformation.makeProfilesCSV(args(0), args(1), args(2), args(3))
+      */
+    val sparkConf =
+      new SparkConf().setMaster("local[2]").setAppName("Fake")
+    val sc = new SparkContext(sparkConf)
+    val hadoopConf = new org.apache.hadoop.conf.Configuration()
+    val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+    val df = sqlContext.read
+      .format("com.databricks.spark.xml")
+      .option("rowTag", "profiles")
+      .load(args(0))
+    println("XXXXXXXXXXXXXXXXXXXXXXXXX")
+    df.printSchema()
+    df.show(100)
+
   }
 }
--- src/main/scala/com/test/core/paintshop/PaintFactory.scala
+++ src/main/scala/com/test/core/paintshop/PaintFactory.scala
@@ -1,30 +1,9 @@
 package com.test.core.paintshop
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 object PaintFactory {
-  case class Profiles(city: Int,
-                      loginName: String,
-                      allDevCnt: String,
-                      activeDevCnt: String,
-                      httpVisitsCnt: Int,
-                      top5Sites: String,
-                      forDay: String,
-                      protoTorrent: String,
-                      protoShareman: String,
-                      protoHttp: String,
-                      protoHttps: String,
-                      protoTotal: String,
-                      protoSip: String,
-                      vkCnt: String,
-                      mailRuCnt: String,
-                      competitorsVisitsCnt: String,
-                      devTypesSmartTv: String,
-                      devTypesIos: String,
-                      devTypesAndroid: String,
-                      devTypesWindowsMobile: String,
-                      top5GameSites: String)
 
   private val sparkConf =
     new SparkConf().setMaster("local[2]").setAppName("Fake")
@@ -33,55 +12,27 @@ object PaintFactory {
   private val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
   private val sqlContext = new org.apache.spark.sql.SQLContext(sc)
 
-  private def writeDfToParquet(df: DataFrame,
+  private def readXml(df: DataFrame,
                                hiveWarehousePath: String,
                                partition: String): Unit = {
 
-    if (hdfs.exists(new org.apache.hadoop.fs.Path(hiveWarehousePath)))
-      df.write
-        .partitionBy(partition)
-        .mode(org.apache.spark.sql.SaveMode.Append)
-        .format("parquet")
-        .save(hiveWarehousePath)
-    else
-      df.write.partitionBy(partition).format("parquet").save(hiveWarehousePath)
-  }
 
-  def writeDataToHive(hiveWarehousePath: String,
-                      inputFilePath: String,
-                      partition: String): Unit = {
 
-    import sqlContext.implicits._
+    val sqlContext = new SQLContext(sc)
+    val df = sqlContext.read
+      .format("com.databricks.spark.xml")
+      .option("rowTag", "prf")
+      .load("books.xml")
 
-    val df = sc
-      .textFile(inputFilePath)
-      .map(_.split(";"))
-      .map(columns =>
-        Profiles(
-          columns(0).toInt,
-          columns(1),
-          columns(2),
-          columns(3),
-          columns(4).toInt,
-          columns(5),
-          columns(6),
-          columns(7),
-          columns(8),
-          columns(9),
-          columns(10),
-          columns(11),
-          columns(12),
-          columns(13),
-          columns(14),
-          columns(15),
-          "A",
-          "B",
-          "C",
-          "D",
-          "E"
-      ))
-      .toDF()
+    df.show(100)
 
-    writeDfToParquet(df, hiveWarehousePath, partition)
+/**
+    val selectedData = df.select("author", "_id")
+    selectedData.write
+      .format("com.databricks.spark.xml")
+      .option("rootTag", "books")
+      .option("rowTag", "book")
+      .save("newbooks.xml")
+  */
   }
 }
